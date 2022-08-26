9df67e30142ff986c65fdb532d7d6abd64e68ea5
Ezequiel Ferrín <ezequiel.ferrin@mercadolibre.com>
2022-08-26 13:38:02 -0300

    session 1

--- app/src/main/java/com/learning/compose/application/MainActivity.kt
+++ app/src/main/java/com/learning/compose/application/MainActivity.kt
@@ -3,11 +3,18 @@ package com.learning.compose.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learning.compose.application.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Greeting("Meli Components")
+                MyApp()
             }
         }
     }
@@ -24,16 +31,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello, $name!")
+private fun MyApp() {
+    Column(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+    ) {
+
+        Greetings(name = "iOs")
+        Greetings(name = "Android")
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greetings(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+
+            Column(
+                Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show More")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        Greeting("Meli Components")
+        MyApp()
+    }
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    ComposeApplicationTheme {
+        Greetings(name = "Meli Component!")
     }
 }
