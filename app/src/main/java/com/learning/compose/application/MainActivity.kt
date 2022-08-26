e511150faf7dace6c245482a4dcaad4bba28f4d8
Ezequiel Ferrín <ezequiel.ferrin@mercadolibre.com>
2022-08-26 02:54:52 -0300

    use property delegate by to assign the state

--- app/src/main/java/com/learning/compose/application/MainActivity.kt
+++ app/src/main/java/com/learning/compose/application/MainActivity.kt
@@ -12,8 +12,10 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,10 +45,10 @@ private fun MyApp(names: List<String> = listOf("Android", "iOS")) {
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember {
+    var expanded by remember {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier
@@ -62,8 +64,8 @@ private fun Greeting(name: String) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show Less" else "Show More")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show Less" else "Show More")
             }
         }
     }
