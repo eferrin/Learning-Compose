2822d48f678af37bca6d4ff73b48dfa1397aef8e
Ezequiel Ferrín <ezequiel.ferrin@mercadolibre.com>
2022-08-26 00:56:50 -0300

    Add Padding

--- app/src/main/java/com/learning/compose/application/MainActivity.kt
+++ app/src/main/java/com/learning/compose/application/MainActivity.kt
@@ -3,11 +3,14 @@ package com.learning.compose.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learning.compose.application.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +23,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 }
 
 @Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello, $name!")
+        Text(text = "Hello, $name!", Modifier.padding(24.dp))
     }
 }
 
