dbc242d2620894251702f54f6c22b28ebe8baafe
Ezequiel Ferrín <ezequiel.ferrin@mercadolibre.com>
2022-08-26 00:31:03 -0300

    setup

--- app/src/main/java/com/learning/compose/application/MainActivity.kt
+++ app/src/main/java/com/learning/compose/application/MainActivity.kt
@@ -3,22 +3,9 @@ package com.learning.compose.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ButtonColors
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.learning.compose.application.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,60 +14,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                MyActivityComposed()
+                Greeting("Meli Components")
             }
         }
     }
 
-}
-
-@Composable
-fun MyActivityComposed(names: List<String> = listOf("World", "Components")) {
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(vertical = 4.dp)
-    ) {
-        for (name in names) {
-            Greeting(name)
-        }
-    }
 }
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    Surface(
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
-        color = MaterialTheme.colors.background
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(14.dp)
-        ) {
-            Column(
-                Modifier
-                    .weight(1f)
-            ) {
-                Text(text = "Hello,")
-                Text(text = "$name!")
-            }
-
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show Less" else "Show More")
-            }
-        }
-    }
+    Text(text = "Hello, $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        MyActivityComposed()
+        Greeting("Meli Components")
     }
 }
